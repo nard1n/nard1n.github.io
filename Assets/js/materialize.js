b36ec7fa1c4008ceac200111b8529ced7105c6a6
Nardin Lachowski <nardin@live.com>
2021-03-01 03:31:03 -0500

    complete portfolio revamp

---
+++ Assets/js/materialize.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', function() {
+    M.AutoInit();
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.parallax');
+    var instances = M.Parallax.init(elems, options);
+  });
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.scrollspy');
+    var instances = M.ScrollSpy.init(elems, options);
+  });
